4c9448c6f96874dfd0b7e307af6a4245bbf7f012
Drew McPherson <drewmac1971@gmail.com>
2019-04-17 23:06:52 -0500

    added model, view, and controller for physical activity

---
+++ PersonalHealthTracker/PersonalHealthTracker.Model/Model/Physical_Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PersonalHealthTracker.Models
+{
+    public class Physical_Activity
+    {
+        public string Description { get; set; }
+        public int Duration { get; set; }
+
+        [Display(Name ="Calories Burned")]
+        public int CaloriesBurned { get; set; }
+
+        [Display(Name ="Day of Week")]
+        public DayOfWeek dayOfWeek { get; set; }
+    }
+}
---
+++ PersonalHealthTracker/PersonalHealthTracker/Controllers/Physical_ActivityController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PersonalHealthTracker.Models;
+
+namespace PersonalHealthTracker.WebUI.Controllers
+{
+    public class Physical_ActivityController : Controller
+    {
+        private List<Physical_Activity> Physical_Activities = new List<Physical_Activity>();
+
+        // Physical_Activity/Index
+        public IActionResult Index()
+        {
+            return View(Physical_Activities);
+        }
+
+        // HTTP GET Physical_Activity/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Physical_Activity newPhysical_Activity)
+        {
+            Physical_Activities.Add(newPhysical_Activity);
+           
+            return View(nameof(Index), Physical_Activities);
+        }
+    }
+}
