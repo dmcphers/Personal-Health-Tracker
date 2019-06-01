93484e0c39ab1d64e3faf0ccdb3ec88da4ea05c0
Drew McPherson <drewmac1971@gmail.com>
2019-06-01 15:40:26 -0500

    first save of rest of models homework

---
+++ PersonalHealthTracker/PersonalHealthTracker.Model/Model/Nutrition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PersonalHealthTracker.Domain.Model
+{
+    class Nutrition
+    {
+        public int Id { get; set; } // for DB purposes to make it be identifiable
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+
+        [Required]
+        [Display(Name = "Calories Burned")]
+        public int CaloriesBurned { get; set; }
+
+        [Required]
+        [Display(Name = "Day of Week")]
+        public DayOfWeek dayOfWeek { get; set; }
+
+        // Fully Defined Relationship for Property Type
+        [Display(Name = "Physical Activity")]
+        public int Physical_Activity_TypeId { get; set; }
+        public Physical_Activity_Type Physical_Activity_Type { get; set; }
+
+        // Fully Defined Relationship for App User
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
